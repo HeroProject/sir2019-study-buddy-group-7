479677523f1251c6d09f9bbb920cd4c7a9dbef72
Gianluca Truda <gianlucatruda@gmail.com>
2019-11-13 17:23:53 +0100

    Added some diagnostics.

--- SampleApplication.py
+++ SampleApplication.py
@@ -1,17 +1,39 @@
 import AbstractApplication as Base
-
+from time import sleep
 
 class SampleApplication(Base.AbstractApplication):
     def main(self):
         self.setLanguage('en-US')
-        self.sayAnimated('Hello, world!')
+        self.sayAnimated('Hello Group 7.')
         sleep(2)
 
     def onRobotEvent(self, event):
         print(event)
 
+class DiagnosticApp(Base.AbstractApplication):
+    def run(self):
+        self.setLanguage('en-US')
+        self.sayAnimated('Running diagnostics now.')
+        sleep(2)
+        '''
+        self.say('Changing eye color')
+        self.setEyeColour('red')
+        sleep(4)
+        self.setEyeColour('blue')
+        sleep(1)
+        '''
+        self.say('Testing mobility.')
+        sleep(2)
+        self.doGesture('BowShort_1')
+        #sleep(4)
+        #self.say('Diagnostics complete.')
+
 
 # Run the application
-sample = SampleApplication()
-sample.main()
-sample.stop()
+#sample = SampleApplication()
+#sample.main()
+#sample.stop()
+
+diagnostic_app = DiagnosticApp()
+diagnostic_app.run()
+diagnostic_app.stop()
