7e07fcc1adec2e64764b75f5e4b0a0ec343a304d
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2019-11-15 17:33:40 +0100

    Vars to camelCase

--- SampleApplication.py
+++ SampleApplication.py
@@ -8,39 +8,39 @@ class SampleApplication(Base.AbstractApplication):
         super().__init__()
 
         # Semaphores for async execution. They make sure that the action is completed.
-        self.gesture_lock = Semaphore(0)
-        self.language_lock = Semaphore(0)
-        self.text_lock = Semaphore(0)
-        self.eyecolor_lock = Semaphore(0)
-        self.name_lock = Semaphore(0)
+        self.gestureLock = Semaphore(0)
+        self.languageLock = Semaphore(0)
+        self.textLock = Semaphore(0)
+        self.eyeColorLock = Semaphore(0)
+        self.nameLock = Semaphore(0)
 
         # Attributes of our application
         # Beware that if naming is changed here, the corresponding parameter in DialogeFlow has to be
         # changed as well if it exists
-        self.user_name = None
+        self.userName = None
 
     def main(self):
         # Setting language
         self.setLanguage('en-US')
-        self.language_lock.acquire()
+        self.languageLock.acquire()
 
         # Change Eyecolor
         self.setEyeColour('red')
-        self.eyecolor_lock.acquire()
+        self.eyeColorLock.acquire()
 
         # Saying something with body language
         self.sayAnimated('Hello Group 7.')
-        self.text_lock.acquire()
+        self.textLock.acquire()
 
         # Say something without moving
         self.say('Boring hello group 7.')
-        self.text_lock.acquire()
+        self.textLock.acquire()
 
         # Do a gesture
         # untitled-2ae403 is the ID of the Project in Choreographe.
         # Did not know this in advance, hence the awful name.
         self.doGesture('untitled-2ae403/behavior_1')
-        self.gesture_lock.acquire()
+        self.gestureLock.acquire()
 
         # Pass the required Dialogflow parameters (add your Dialogflow parameters)
         self.setDialogflowKey('sample_diagFl_key.json')
@@ -50,23 +50,25 @@ class SampleApplication(Base.AbstractApplication):
         # This snipped assumes a DialogueFlow application listening for a name
         self.setAudioContext('answer_name')
         self.startListening()
-        self.name_lock.acquire(timeout=5)
+        self.nameLock.acquire(timeout=5)
         self.stopListening()
 
     def onAudioIntent(self, *args, intentName):
         # Assuming we have a DialogueFlow app for the intent "name"
         if intentName == 'answer_name' and len(args) > 0:
-            self.user_name = args[0]
-            self.name_lock.release()
+            self.userName = args[0]
+            self.nameLock.release()
 
     def onRobotEvent(self, event):
         # make sure all our started actions are completed:
         if event == 'GestureDone':
-            self.gesture_lock.release()
+            self.gestureLock.release()
         elif event == 'TextDone':
-            self.text_lock.release()
+            self.textLock.release()
         elif event == 'LanguageChanged':
-            self.language_lock.release()
+            self.languageLock.release()
+        elif event == 'EyeColourDone':
+            self.eyeColorLock.release()
 
         # print what is going on
         print(event)
